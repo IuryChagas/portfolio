efe2fbcc566eadbd64c76611187798aff781ab63
Iury Chagas <iurychagas@icloud.com>
2020-01-24 16:02:32 -0300

    add 1° versão do projeto finalizado!

---
+++ Portfolio/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portfolio.Models;
+
+namespace Portfolio.Controllers {
+
+    //[Route("/{action=index}")]
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult About() {
+            return View();
+        }
+        public IActionResult Setup() {
+            return View();
+        }
+        public IActionResult Contact() {
+            return View();
+        }
+
+
+    }
+}
